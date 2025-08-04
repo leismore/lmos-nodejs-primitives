95d25d5666f8c22bd10a4fa26778fb2d3b3c703a
Kyle Chine <kyle.chine@leismore.co>
2025-08-04 10:49:15 +1000

    Add ZERO and ZERO_NEGATIVE / BIGINT_ZERO and BIGINT_ZERO_NEGATIVE

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,14 @@ const INTEGER          =  42;
 const INTEGER_NEGATIVE = -42;
 const FLOAT            =  3.14;
 const FLOAT_NEGATIVE   = -3.14;
+const ZERO             = 0;
+const ZERO_NEGATIVE    = -0;
 
 // BigInt
-const BIGINT          = BigInt(Number.MAX_SAFE_INTEGER) + BigInt(1);
-const BIGINT_NEGATIVE = BigInt(Number.MIN_SAFE_INTEGER) - BigInt(1);
+const BIGINT               = BigInt(Number.MAX_SAFE_INTEGER) + BigInt(1);
+const BIGINT_NEGATIVE      = BigInt(Number.MIN_SAFE_INTEGER) - BigInt(1);
+const BIGINT_ZERO          = BigInt(0);
+const BIGINT_ZERO_NEGATIVE = BigInt(-0);
 
 // String
 const STRING           = 'Hello, World!';
@@ -38,11 +42,13 @@ const SYMBOL = Symbol();
 
 // Export all defined primitive values
 export {
-    NULL    ,    UNDEFINED        ,
-    TRUE    ,    FALSE            ,
-    INTEGER ,    INTEGER_NEGATIVE ,
-    FLOAT   ,    FLOAT_NEGATIVE   ,
-    BIGINT  ,    BIGINT_NEGATIVE  ,
-    STRING  ,    STRING_EMPTY     ,    STRING_MULTILINE,
+    NULL        ,    UNDEFINED            ,
+    TRUE        ,    FALSE                ,
+    INTEGER     ,    INTEGER_NEGATIVE     ,
+    FLOAT       ,    FLOAT_NEGATIVE       ,
+    ZERO        ,    ZERO_NEGATIVE        ,
+    BIGINT      ,    BIGINT_NEGATIVE      ,
+    BIGINT_ZERO ,    BIGINT_ZERO_NEGATIVE ,
+    STRING      ,    STRING_EMPTY         ,    STRING_MULTILINE,
     SYMBOL
 };
